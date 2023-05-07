292547849482219fa0082186f5382231e1c252a3
19ShengSheng <1970147020@qq.com>
2023-05-07 22:01:31 +0800

    --story=1018177 --user=张灵冉

---
+++ main/ware/java/com/xunqi/gulimall/ware/vo/MergeVo.java
@@ -0,0 +1,16 @@
+package com.xunqi.gulimall.ware.vo;
+
+import lombok.Data;
+
+import java.util.List;
+
+
+
+@Data
+public class MergeVo {
+
+    private Long purchaseId;
+
+    private List<Long> items;
+
+}
